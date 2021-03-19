ec9a945a493ba019e968aad2a521f100a5f71a52
chasscepts <francischassobetta@gmail.com>
2021-03-19 22:02:21 +0100

    refactor methods to pass failing tests

--- enumerables.rb
+++ enumerables.rb
@@ -1,45 +1,50 @@
 module Enumerable
   def my_each
-    array = to_a
-    array.size.times { |index| yield array[index] }
-    array
+    if block_given?
+      array = to_a
+      array.size.times { |index| yield array[index] }
+      return self
+    end
+    to_enum
   end
 
   def my_each_with_index
-    array = to_a
-    array.size.times { |index| yield(array[index], index) }
-    array
+    if block_given?
+      array = to_a
+      array.size.times { |index| yield(array[index], index) }
+      return self
+    end
+    to_enum
   end
 
   def my_select
-    result = []
-    my_each { |item| result << item if yield item }
-    result
+    if block_given?
+      result = []
+      my_each { |item| result << item if yield item }
+      result
+    end
+    to_enum
   end
 
-  def my_all?(pattern = nil)
-    if block_given?
-      my_each { |value| return false unless yield(value) }
-      return true
+  def my_all?(pattern = nil, &block)
+    if block_given? || pattern.nil?
+      helper = block_given? ? block : proc { |value| value }
+      my_each { |value| return false unless helper.call(value) }
+    else
+      my_each { |value| return false unless match_pattern?(value, pattern) }
     end
-    unless pattern.nil
-      my_each { |value| return false unless match_pattern(value, pattern) }
-      return true
-    end
-    my_each { |value| return false unless value }
+
     true
   end
 
   def my_any?(pattern = nil)
-    if block_given?
-      my_each { |item| return true if yield(item) }
-      return false
+    if block_given? || pattern.nil?
+      helper = block_given? ? block : proc { |value| value }
+      my_each { |value| return true if helper.call(value) }
+    else
+      my_each { |value| return true if match_pattern?(value, pattern) }
     end
-    unless pattern.nil?
-      my_each { |item| return true if match_pattern(item, pattern) }
-      return false
-    end
-    my_each { |item| return true if item }
+
     false
   end
 
@@ -49,42 +54,36 @@ module Enumerable
       return true
     end
     unless pattern.nil?
-      my_each { |value| return false if match_pattern(value, pattern) }
+      my_each { |value| return false if match_pattern?(value, pattern) }
       return true
     end
-    my_each { |_value| return false if item }
+    my_each { |value| return false if value }
     true
   end
 
   def my_count(item = nil)
     items_count = 0
     if !item.nil?
-      my_each { items_count += 1 if value == item }
+      my_each { |value| items_count += 1 if value == item }
     elsif block_given?
-      my_each { items_count += 1 if yield(value) }
+      my_each { |value| items_count += 1 if yield(value) }
     else
       my_each { items_count += 1 }
     end
     items_count
   end
 
-  def my_map(proc = nil)
-    new_array = []
-    if !proc.nil
-      my_each { |value| new_array << proc.call(value) }
-      new_array
-    elsif block_given?
-      my_each { |value| new_array << yield(value) }
-      new_array
-    else
-      to_enum
-    end
+  def my_map(proc = nil, &block)
+    return my_map_helper(proc) unless proc.nil?
+    return my_map_helper(block) if block_given?
+
+    to_enum
   end
 
   def my_inject(*args)
     array = to_a
 
-    return my_inject_with_block(array, args) if block_given?
+    return my_inject_with_block(array, args) { |memo, item| yield(memo, item) } if block_given?
 
     raise LocalJumpError, 'no block given' if args.empty?
 
@@ -99,14 +98,11 @@ module Enumerable
 
   private
 
-  def match_pattern(item, pattern)
-    return true if item == pattern || item =~ pattern
+  def match_pattern?(item, pattern)
+    return true if pattern.is_a?(Regexp) && item =~ pattern
+    return true if pattern.is_a?(Class) && item.is_a?(pattern)
+    return true if item == pattern
 
-    begin
-      return true if item.is_a?(pattern)
-    rescue TypeError
-      return false
-    end
     false
   end
 
@@ -117,6 +113,12 @@ module Enumerable
     (index...array.size).my_each { |idx| memo = yield(memo, array[idx]) }
 
     memo
+  end
+
+  def my_map_helper(block_or_proc)
+    new_array = []
+    my_each { |value| new_array << block_or_proc.call(value) }
+    new_array
   end
 end
 
