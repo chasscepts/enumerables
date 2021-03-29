0c860d9c001a0fcec58ac6c49fc1da80c0f20038
garciajordy <jordygarcia0119@gmail.com>
2021-03-29 16:46:25 +0300

    Add test case for my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,10 +1,18 @@
 require "./enumerables"
 
 describe Enumerable do
-  describe "my_each" do
+  describe "#my_each" do
     it "puts every item in the array" do
       array = [1, 2, 3, 4]
       expect(array.my_each { |item| puts item }).to eql([1, 2, 3, 4])
+    end
+  end
+
+  describe "#my_each_with_index" do
+    it "puts every item with the index" do
+      array = ["jordy","addis","hanna"]
+      expect(array.my_each_with_index{|item,index| puts "#{index+1}. #{item}"}).to eql(["jordy", "addis", "hanna"]
+      )
     end
   end
 end
