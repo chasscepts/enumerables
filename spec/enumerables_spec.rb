118553880a8c5251ddfcc4a50995bb8088a9f4dc
garciajordy <jordygarcia0119@gmail.com>
2021-03-29 17:04:47 +0300

    Add my_all? test for rspec

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -14,10 +14,34 @@ describe Enumerable do
       expect(array.my_each_with_index { |item, index| puts "#{index + 1}. #{item}" }).to eql(["jordy", "addis", "hanna"])
     end
   end
+
   describe "#my_select" do
     it "puts selected item greater than two" do
       array = [1, 2, 3]
       expect(array.my_select { |item| item > 2 }).to eql([3])
     end
   end
+
+  describe "#my_all?" do
+    it "return false if not all items are greater than 3"do
+    array = [1, 2, 3]
+    expect(array.my_all? { |item| item > 3 }).to eql(false)
+  end
+
+  it "returns true if all items are greater than 0"do
+  array = [1, 2, 3]
+  expect(array.my_all? { |item| item > 0 }).to eql(true)
+  end
+
+  it "returns true if all items are Numerics"do
+  array = [1, 2, 3]
+  expect(array.my_all?(Numeric)).to eql(true)
+  end
+
+  it "returns true if all items contain the letter 'd'"do
+  array = ["jordy", "addis", "hannad"]
+  expect(array.my_all?(/d/)).to eql(true)
+  end
+  end
+
 end
